88847f12e976e78c71cd5a1e40328d3cece5b7ab
Wruzz Jen-Iisk <43224790+GentlePuppet@users.noreply.github.com>
2022-09-17 21:59:22 -0400

    Version 3
    - Cleaned up the plugin.
    - Properly disable plugin when disabled in the BD plugin settings.

--- FullSizeAvatars.plugin.js
+++ FullSizeAvatars.plugin.js
@@ -1,8 +1,8 @@
 /**
- * @name Full Res Avatars
+ * @name FullResAvatars
  * @author GentlePuppet
  * @authorId 199263542833053696
- * @version 2.2
+ * @version 3.0
  * @description Hover over avatars to see a bigger version.
  * @source https://github.com/GentlePuppet/FullResAvatar.plugin.js/blob/main/FullSizeAvatars.plugin.js
  * @updateUrl https://raw.githubusercontent.com/GentlePuppet/FullResAvatar.plugin.js/main/FullSizeAvatars.plugin.js
@@ -10,26 +10,21 @@
  
  const config = {
 	info: {
-		name: "Full Res Avatars",
-		id: "FullResAvatarHover",
-		description: "Hover over avatars to see a bigger version.",
-		version: "2.2",
-		author: "GentlePuppet",
-		updateUrl: "https://raw.githubusercontent.com/GentlePuppet/FullResAvatar.plugin.js/main/FullSizeAvatars.plugin.js"
+		version: "3.0",
+		updateUrl: "https://raw.githubusercontent.com/GentlePuppet/FullResAvatar.plugin.js/main/FullSizeAvatars.plugin.js",
 	}
 };
  
-module.exports = class FullResAvatars {
-	
-	getName() { return config.info.name; }
-	
+module.exports = class {
+	//---- Try to load ZeresPluginLibrary to enable auto-updates
 	load() {
 		try { global.ZeresPluginLibrary.PluginUpdater.checkForUpdate(config.info.name, config.info.version, config.info.updateUrl); }
-		catch (err) { console.error(this.getName(), "Failed to reach the ZeresPluginLibrary for Plugin Updater.", err); }
+		catch (err) { console.error(this.getName(), "Failed load the ZeresPluginLibrary for automatic updates.", err); }
 	}
-
+	
+	//---- Start Plugin
     start() {
-		//----Create Image Panel
+		//---- Create Image Panel
 		if (document.getElementById("ImagePanelHover")) {
 			document.getElementById("ImagePanelHover").remove();
 		}
@@ -37,48 +32,60 @@ module.exports = class FullResAvatars {
 		document.body.after(imagepanel);
 		imagepanel.setAttribute("id", "ImagePanelHover");
 		imagepanel.setAttribute("src", "null");
-		imagepanel.setAttribute("style", "top: 50%;left:50%;height:205px;width:205px;padding:5px;display:none;z-index:999999;opacity:1;background:var(--background-secondary);border-size:5px;border-style:solid;position:absolute;");
+		imagepanel.setAttribute("style", "top: 50%;left:50%;height:261px;width:261px;padding:5px;display:none;z-index:999999;opacity:1;background:var(--background-secondary);border-size:5px;border-style:solid;position:absolute;");
 
-		//---- Create Avatar Hover Events
-		document.addEventListener("mouseover", hoverevent);
-		function hoverevent() {
-			document.querySelectorAll(".avatar-6qzftW, .avatar-1HDIsL, .avatar-2MSPKk").forEach(function(avatar) {
-				avatar.onmouseover = ahoverover;
-				avatar.onmouseout = ahoverout;
-			})
-		}
-		
-		//---- Track Mouse Position
-		document.addEventListener("mousemove", funcmousemove) 
-		function funcmousemove(e){
-			if (document.querySelector(".avatar-6qzftW:hover")) {
-				document.querySelector("#ImagePanelHover").style.top = e.pageY - 10 + 'px';
-				document.querySelector("#ImagePanelHover").style.left = e.pageX - 220 + 'px';
-			}
-			else if (document.querySelector(".avatar-1HDIsL:hover") || document.querySelector(".avatar-2MSPKk:hover")) {
-				document.querySelector("#ImagePanelHover").style.top = e.pageY - 10 + 'px';
-				document.querySelector("#ImagePanelHover").style.left = e.pageX + 20 + 'px';
-			}
-		};
-		
-		//---- Start Hovering
-		function ahoverover(jnode){
-			var avatarsrc = document.querySelector("div:hover > div > svg > foreignObject > div > img").src.replace('?size=32', '?size=256');
-			var status = document.querySelector("div:hover > div > svg > rect").getAttribute('fill');
-			
-			document.querySelector("#ImagePanelHover").style.display = "block";
-			document.querySelector("#ImagePanelHover").src = avatarsrc;
-			document.querySelector("#ImagePanelHover").style.background = status;
-			//document.querySelector("#ImagePanelHover").style.top = CurrentMouseYPostion;
-			//document.querySelector("#ImagePanelHover").style.left = CurrentMouseXPostion;
-		};
-		
-		//---- Stop Hovering
-		function ahoverout(){
-			document.querySelector("#ImagePanelHover").style.display = "none";
-			document.querySelector("#ImagePanelHover").src = "null";
-		};
+		//---- Create Track Mouse Event
+		document.addEventListener("mousemove", this.mmhfunc) 
     };
+		
+	//---- Track Mouse Event Handler
+	mmhfunc = (e) => this.funcmousemove(e);
 	
-    stop() {};
+    stop() {
+		document.removeEventListener('mousemove', this.mmhfunc);
+		document.getElementById("ImagePanelHover").remove();
+	};
+	
+	//---- Track Mouse Event and Check If Hovering Over Avatars
+	funcmousemove(e){
+		let mah = document.querySelector(".avatar-6qzftW:hover")
+		let fah = document.querySelector(".avatar-1HDIsL:hover")
+		let fadmh = document.querySelector(".avatar-2MSPKk:hover")
+		let ipm = document.querySelector("#ImagePanelHover")
+		
+		if (mah == null || fah == null || fadmh == null ) {
+			ipm.style.display = "none";
+			ipm.src = "null";
+		}
+		if (mah) {
+			var ais = document.querySelector("div:hover > div > svg > foreignObject > div > img").src.replace('?size=32', '?size=256');
+			var status = document.querySelector("div:hover > div > svg > rect").getAttribute('fill');
+			ipm.src = ais;
+			ipm.style.display = "block";
+			ipm.style.top = e.pageY - 10 + 'px';
+			ipm.style.left = e.pageX - 286 + 'px';
+			if (status == "transparent") {
+				var statusfix = document.querySelector("div:hover > div > svg > svg > rect").getAttribute('fill');
+				ipm.style.background = statusfix;
+			} else {
+				var status = document.querySelector("div:hover > div > svg > rect").getAttribute('fill');
+				ipm.style.background = status;
+			}
+		}
+		else if (fah || fadmh) {
+			var ais = document.querySelector("div:hover > div > svg > foreignObject > div > img").src.replace('?size=32', '?size=256');
+			var status = document.querySelector("div:hover > div > svg > rect").getAttribute('fill');
+			ipm.src = ais;
+			ipm.style.display = "block";
+			ipm.style.top = e.pageY - 10 + 'px';
+			ipm.style.left = e.pageX + 20 + 'px';
+			if (status == "transparent") {
+				var statusfix = document.querySelector("div:hover > div > svg > svg > rect").getAttribute('fill');
+				ipm.style.background = statusfix;
+			} else {
+				var status = document.querySelector("div:hover > div > svg > rect").getAttribute('fill');
+				ipm.style.background = status;
+			}
+		}
+	};
 };
